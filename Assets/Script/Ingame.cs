4a59e487b334aee26a4f445aca1666ab14697859
ttok14 <ttok14@naver.com>
2022-09-24 11:43:37 +0900

    [InGame 시뮬레이션 스크립트 작성]

---
+++ Assets/Script/Ingame.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.UI;
+
+public class Ingame : MonoBehaviour
+{
+    public Image img;
+    public Text txtResult;
+
+    public string TargetSpriteKey;
+
+    private void Start()
+    {
+        Addressables.LoadAssetAsync<Sprite>(TargetSpriteKey).Completed += (result) =>
+        {
+            if (result.Status == UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationStatus.Succeeded)
+            {
+                txtResult.text = "성공!";
+                img.sprite = result.Result;
+            }
+            else
+            {
+                txtResult.text = "실패!";
+                img.sprite = null;
+            }
+        };
+    }
+}
